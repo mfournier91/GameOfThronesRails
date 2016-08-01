6293e56c26298b4937d295437134b6605b98d342
Marc Fournier <marc.fournier91@gmail.com>
2016-08-01 18:16:44 -0400

    House index and show are now functional

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,6 +1,10 @@
 class HousesController < ApplicationController
-
+# A frequent practice is to place the standard CRUD actions in each controller in the following order: index, show, new, edit, create, update and destroy.
   def index
     @houses = House.all
+  end
+
+  def show
+    @house = House.find(params[:id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,18 +14,20 @@ lannister = House.create!({name: 'Lannister', words: 'A Lannister always pays hi
 tyrell = House.create!({name: 'Tyrell', words: 'Growing Strong', sigil_url: 'http://vignette2.wikia.nocookie.net/gameofthrones/images/c/cf/House-Tyrell-Main-Shield.PNG/revision/latest?cb=20151209185350'})
 
 puts "Houses created"
-Character.create!(name: 'Daenerys Targaryen', photo_url: 'http://vignette2.wikia.nocookie.net/gameofthrones/images/2/28/Dany_finale_s6_winds_of_winter.jpg/revision/latest?cb=20160630224410', house_id: targaryen.id)
+Character.create!(name: 'Daenerys Targaryen', photo_url: 'http://vignette2.wikia.nocookie.net/gameofthrones/images/2/28/Dany_finale_s6_winds_of_winter.jpg/revision/latest?cb=20160630224410', house: targaryen)
+puts "Daenerys created"
 
-# Character.create!({name: "Ned Stark", photo_url: 'http://vignette1.wikia.nocookie.net/gameofthrones/images/3/37/Eddard_Stark_infobox_new.jpg/revision/latest?cb=20160730050722', house_id: stark.id})
-# Character.create!({name: "Sansa Stark", photo_url: 'http://vignette1.wikia.nocookie.net/gameofthrones/images/c/cf/Sansa_Battle_of_Bastards_main.jpg/revision/latest?cb=20160621005428', house_id: stark.id})
-# Character.create!({name: "Arya Stark", photo_url: 'http://vignette1.wikia.nocookie.net/gameofthrones/images/a/ad/Arya6x10.png/revision/latest?cb=20160629211505', house_id: stark.id})
-# Character.create!({name: "Bran Stark", photo_url: 'http://vignette1.wikia.nocookie.net/gameofthrones/images/0/0d/Bran_winds_of_winter_finale_s6.jpg/revision/latest?cb=20160623043335', house_id: stark.id})
-#
-#
-# Character.create!({name: "Cersei Lannister", photo_url: 'http://vignette2.wikia.nocookie.net/gameofthrones/images/c/c7/Queen_Cersei_Main_The_winds_of_Winter.jpg/revision/latest?cb=20160712032316', house_id: lannister.id})
-# Character.create!({name: "Jaime Lannister", photo_url: 'http://vignette4.wikia.nocookie.net/gameofthrones/images/c/c5/Jaime_s6_Ep08_.jpg/revision/latest?cb=20160616044155', house_id: lannister.id})
-# Character.create!({name: "Tyrion Lannister", photo_url: 'http://vignette4.wikia.nocookie.net/gameofthrones/images/9/9d/Tyrion6x082.png/revision/latest?cb=20160609061404', house_id: lannister.id})
-#
-#
-# Character.create!({name: "Margaery Tyrell", photo_url: 'http://vignette2.wikia.nocookie.net/gameofthrones/images/b/b5/Margaery_Tyrell_S6.png/revision/latest?cb=20160729224605', house_id: tyrell.id})
+Character.create!({name: "Ned Stark", photo_url: 'http://vignette1.wikia.nocookie.net/gameofthrones/images/3/37/Eddard_Stark_infobox_new.jpg/revision/latest?cb=20160730050722', house: stark})
+Character.create!({name: "Sansa Stark", photo_url: 'http://vignette1.wikia.nocookie.net/gameofthrones/images/c/cf/Sansa_Battle_of_Bastards_main.jpg/revision/latest?cb=20160621005428', house: stark})
+Character.create!({name: "Arya Stark", photo_url: 'http://vignette1.wikia.nocookie.net/gameofthrones/images/a/ad/Arya6x10.png/revision/latest?cb=20160629211505', house: stark})
+Character.create!({name: "Bran Stark", photo_url: 'http://vignette1.wikia.nocookie.net/gameofthrones/images/0/0d/Bran_winds_of_winter_finale_s6.jpg/revision/latest?cb=20160623043335', house: stark})
+
+
+Character.create!({name: "Cersei Lannister", photo_url: 'http://vignette2.wikia.nocookie.net/gameofthrones/images/c/c7/Queen_Cersei_Main_The_winds_of_Winter.jpg/revision/latest?cb=20160712032316', house: lannister})
+Character.create!({name: "Jaime Lannister", photo_url: 'http://vignette4.wikia.nocookie.net/gameofthrones/images/c/c5/Jaime_s6_Ep08_.jpg/revision/latest?cb=20160616044155', house: lannister})
+Character.create!({name: "Tyrion Lannister", photo_url: 'http://vignette4.wikia.nocookie.net/gameofthrones/images/9/9d/Tyrion6x082.png/revision/latest?cb=20160609061404', house: lannister})
+
+
+Character.create!({name: "Margaery Tyrell", photo_url: 'http://vignette2.wikia.nocookie.net/gameofthrones/images/b/b5/Margaery_Tyrell_S6.png/revision/latest?cb=20160729224605', house: tyrell})
+
 #
