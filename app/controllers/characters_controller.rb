b980968d3395842c0eee20a0a05c2d126ede7dec
Marc Fournier <marc.fournier91@gmail.com>
2016-08-02 11:38:58 -0400

    Adds functionality for new characters.

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -6,4 +6,21 @@ class CharactersController < ApplicationController
     @character = Character.find(params[:id])
   end
 
+  def new
+    @character = Character.new
+  end
+
+  def create
+    @character = Character.new character_params
+    if @character.save
+      redirect_to @character
+    end
+  end
+
+
+private
+def character_params
+  params.require(:character).permit(:name, :photo_url, :house_id)
+end
+
 end
