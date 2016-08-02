aabdb9fd997c3919316c51a97d79aa24b23b6b5d
Marc Fournier <marc.fournier91@gmail.com>
2016-08-02 11:52:19 -0400

    Adds edit functionality to characters.

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -10,10 +10,23 @@ class CharactersController < ApplicationController
     @character = Character.new
   end
 
+  def edit
+    @character = Character.find(params[:id])
+  end
+
   def create
     @character = Character.new character_params
     if @character.save
       redirect_to @character
+    end
+  end
+
+  def update
+    @character = Character.find(params[:id])
+    if @character.update(character_params)
+      redirect_to @character
+    else
+      render 'edit'
     end
   end
 
